9467f0923c4628d96c18535158ff42f8f0bde3a3
david-lafontant <lafontant.david@gmail.com>
2023-05-26 16:01:55 -0400

    Create book category test file

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it {should_have_many(:books)}
+  it {should_validate_presence_of(:name)}
+  it {should_validate_length_of(:name).is_at_least(3).is_at_most(60)}
 end
