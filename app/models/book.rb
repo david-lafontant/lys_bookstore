d5b8265d6733356e979b46650be5fe00852be65b
david-lafontant <lafontant.david@gmail.com>
2023-05-26 16:19:27 -0400

    Setup book model validations and association

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,5 @@
 class Book < ApplicationRecord
   belongs_to :category
+  validates :title, presence: true, length: {minimum: 3, maximum: 100}
+  validates :author, presence: true, length: {minimum: 3, maximum: 100} 
 end
