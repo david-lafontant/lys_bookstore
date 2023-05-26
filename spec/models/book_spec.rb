5e1344327a5c42de9cfe07593a5c7e961c549e06
david-lafontant <lafontant.david@gmail.com>
2023-05-26 16:17:24 -0400

    Fix typos in test files

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-it {should_belong_to(:category)}
-it {should_validate_presence_of(:title)}
-it {should_validate_presence_of(:author)}
-it {should_validate_length_of(:author).is_at_least(3).is_at_most(100)}
-it {should_validate_lenght_of(:title).is_at_least(3).is_at_most(100)}
+  it {should belong_to(:category)}
+  it {should validate_presence_of(:title)}
+  it {should validate_presence_of(:author)}
+  it {should validate_length_of(:author).is_at_least(3).is_at_most(100)}
+  it {should validate_length_of(:title).is_at_least(3).is_at_most(100)}
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  it {should_have_many(:books)}
-  it {should_validate_presence_of(:name)}
-  it {should_validate_length_of(:name).is_at_least(3).is_at_most(60)}
+  it {should have_many(:books)}
+  it {should validate_presence_of(:name)}
+  it {should validate_length_of(:name).is_at_least(3).is_at_most(60)}
 end
