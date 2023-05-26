ee32957ce397efc7142cbec32f8ecff764ce999b
david-lafontant <lafontant.david@gmail.com>
2023-05-26 16:01:20 -0400

    Create book model test file

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+it {should_belong_to(:category)}
+it {should_validate_presence_of(:title)}
+it {should_validate_presence_of(:author)}
+it {should_validate_length_of(:author).is_at_least(3).is_at_most(100)}
+it {should_validate_lenght_of(:title).is_at_least(3).is_at_most(100)}
 end
