55844340302b4fae02f6fb01e0531a070d5e4d2c
david-lafontant <lafontant.david@gmail.com>
2023-05-27 09:38:34 -0400

    Fix typos in bots

--- spec/factories/book.rb
+++ spec/factories/book.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
-  factory :book do
-    title { Faker::Book.title }
-    author { Faker::Book.author }
-    actegory { create(:category) }
-  end
+ factory :book do
+      title { Faker::Book.title }
+   author { Faker::Book.author }
+   category { create(:category) }
+ end
 end
