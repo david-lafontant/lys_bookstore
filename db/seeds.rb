3cabfb143fda0786b77d5768cdc49afa64f09715
david-lafontant <lafontant.david@gmail.com>
2023-05-27 17:40:05 -0400

    Comment out databse seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+#categories = Category.create([{name: 'history'} , {name: 'novel'}])
+# @history = Category.create!(name: 'history')
+# novel = Category.create!(name: 'novel')
+# Book.create!(title: 'world war two', author: 'keegan', category_id: 1)
+# Book.create!(title: 'Memoires de guerre', author: 'churchill', category_id: 1)
+# Book.create!(title: 'red army', author: 'lopez', category_id: 1)
+# Book.create!(title: 'wermacht', author: 'lopez', category_id: 1)
